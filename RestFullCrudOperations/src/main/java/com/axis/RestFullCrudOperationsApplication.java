4a770b55828d2dd1edbeafdf11e7eb3cd60c9136
Sri3195 <n160629@rguktn.ac.in>
2022-12-08 09:29:57 +0530

    spring crud operations

---
+++ RestFullCrudOperations/src/main/java/com/axis/RestFullCrudOperationsApplication.java
@@ -0,0 +1,13 @@
+package com.axis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFullCrudOperationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestFullCrudOperationsApplication.class, args);
+	}
+
+}
